5c71adae4bc5d215a818ef0b13db84fff762853b
alex <7526878@gmail.com>
2025-04-17 14:29:22 +0300

    Back end

---
+++ lib/data/repositories/user/auth_repository.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class AuthenticationRepository extends StatefulWidget {
+  const AuthenticationRepository({super.key});
+
+  @override
+  State<AuthenticationRepository> createState() => _AuthenticationRepositoryState();
+}
+
+class _AuthenticationRepositoryState extends State<AuthenticationRepository> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
